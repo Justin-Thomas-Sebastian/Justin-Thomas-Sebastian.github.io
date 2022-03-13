d675046fff08ced4fc7d834dcf0c9e0926742625
Justin Sebastian <justinthomassebastian@gmail.com>
2022-03-12 23:03:25 -0700

    fixed movement bug when L/R keys are pressed simultaneously

--- js/animation.js
+++ js/animation.js
@@ -8,42 +8,48 @@ const movementKeys = {
 }
 
 let xOffSet = 0;
+const xOffSetMax = 265;
+const wallSize = 100;
+
 function animateCharacter(character, bgImage){
     context.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
     bgImage.renderBackground();
     character.updateLocation();
 
     // move character
-    if(movementKeys.right.pressed && character.position.x <= gameCanvas.width - character.width - 100){
-        character.velocity.x = 10;
-        xOffSet++;
-        console.log("char position:" + character.position.x);
-        console.log("xOffSet:" + xOffSet);
-    } else if (movementKeys.left.pressed && character.position.x >= 0 + 100){
-        character.velocity.x = -10;
-        xOffSet--;
-        console.log("char position:" + character.position.x);
-        console.log("xOffSet:" + xOffSet);
+    if(movementKeys.right.pressed && !movementKeys.left.pressed && 
+       character.position.x <= gameCanvas.width - character.width - wallSize && xOffSet <= xOffSet){
+            character.velocity.x = 10;
+            xOffSet++;
+            console.log("char position:" + character.position.x);
+            console.log("xOffSet:" + xOffSet);
+    } else if (movementKeys.left.pressed && !movementKeys.right.pressed && 
+        character.position.x >= 0 + wallSize){
+            character.velocity.x = -10;
+            xOffSet--;
+            console.log("char position:" + character.position.x);
+            console.log("xOffSet:" + xOffSet);
     } else {
         character.velocity.x = 0;
     }
     
     // scroll background
-    if(movementKeys.right.pressed && 
-        character.position.x >= gameCanvas.width - character.width - 100 &&
-        xOffSet <= 265){
-        xOffSet++;
-        bgImage.position.x -= 10;
-        console.log(gameCanvas.width)
-        console.log("xOffSet:" + xOffSet);
-        console.log("move bg image right");
-    } else if (movementKeys.left.pressed && character.position.x <= 0  + 100 && bgImage.position.x < 0){
-        xOffSet--;
-        bgImage.position.x += 10;
-        console.log("xOffSet:" + xOffSet);
-        console.log("bgImage posish: " + bgImage.position.x);
+    if(movementKeys.right.pressed && !movementKeys.left.pressed &&
+        character.position.x >= gameCanvas.width - character.width - wallSize && xOffSet <= xOffSetMax){
+            xOffSet++;
+            bgImage.position.x -= 10;
+            console.log("char position:" + character.position.x);
+            console.log("xOffSet:" + xOffSet);
+            console.log("move bg image right");
+    } else if (movementKeys.left.pressed && !movementKeys.right.pressed &&
+        character.position.x <= 0  + wallSize && 
+        bgImage.position.x < 0){
+            xOffSet--;
+            bgImage.position.x += 10;
+            console.log("char position:" + character.position.x);
+            console.log("xOffSet:" + xOffSet);
+            console.log("bgImage posish: " + bgImage.position.x);
     }
-
 
     // stop movement if both left and right keys are pressed simultaneously
     if(movementKeys.right.pressed && movementKeys.left.pressed){
